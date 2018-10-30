40b525a240c4f363ef67d433742f548fd94c8bd5
Jafar Al-Gharaibeh <jafar@atcorp.com>
2018-10-30 16:56:11 -0500

    Mazda: initial port
    Signed-off-by: Jafar Al-Gharaibeh <to.jafar@gmail.com>

--- selfdrive/boardd/boardd.cc
+++ selfdrive/boardd/boardd.cc
@@ -41,6 +41,8 @@
 #define SAFETY_CADILLAC 6
 #define SAFETY_HYUNDAI 7
 #define SAFETY_TESLA 8
+#define SAFETY_CHRYSLER 9
+#define SAFETY_MAZDA 10
 #define SAFETY_TOYOTA_IPAS 0x1335
 #define SAFETY_TOYOTA_NOLIMITS 0x1336
 #define SAFETY_ALLOUTPUT 0x1337
@@ -119,6 +121,9 @@ void *safety_setter_thread(void *s) {
     break;
   case (int)cereal::CarParams::SafetyModels::HYUNDAI:
     safety_setting = SAFETY_HYUNDAI;
+    break;
+  case (int)cereal::CarParams::SafetyModels::MAZDA:
+    safety_setting = SAFETY_MAZDA;
     break;
   default:
     LOGE("unknown safety model: %d", safety_model);
