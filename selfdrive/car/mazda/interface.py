214ec3028f25aeb9647bd681fb77c3da7573d22a
Jafar Al-Gharaibeh <to.jafar@gmail.com>
2019-04-23 08:35:32 -0500

    get Kp and Ki close to each other
    Signed-off-by: Jafar Al-Gharaibeh <to.jafar@gmail.com>

--- selfdrive/car/mazda/interface.py
+++ selfdrive/car/mazda/interface.py
@@ -78,7 +78,7 @@ class CarInterface(object):
       ret.steerRatio = 15.5
       ret.steerKf = 0.00006
       ret.steerKiBP, ret.steerKpBP = [[0.], [0.]]
-      ret.steerKpV, ret.steerKiV = [[0.55], [0.15]]
+      ret.steerKpV, ret.steerKiV = [[0.2], [0.18]]
       ret.steerMaxBP = [0.] # m/s
       ret.steerMaxV = [1.]
       tire_stiffness_factor = 0.70
